90eccc570515946349f0bf954db7268ea0741690
JannatunNazifa <nazifajannatun@gmail.com>
2023-03-09 16:13:29 -0500

    Change color attribute from red to hotpink

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function changeColor() {
 	let color = document.body.style.color;
 	if (color === 'black') {
-		document.body.style.color = 'red';
+		document.body.style.color = 'hotpink';
 	} else {
 		document.body.style.color = 'black';
 	}
